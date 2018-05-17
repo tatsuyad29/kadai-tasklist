78ec5fc69d011e6de79e1f819e0d1484c9c5ccac
tatsuya.domoto <ec2-user@ip-172-31-18-183.us-east-2.compute.internal>
2018-05-17 07:10:22 +0000

    complete

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -4,7 +4,69 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Task;
+
 class TasksController extends Controller
 {
-    //
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index', [
+                'tasks' => $tasks
+            ]);
+    }
+    
+    public function show($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.show', [
+                'task' => $task
+            ]);
+    }
+    
+    public function create()
+    {
+        $task = new task;
+        
+        return view('tasks.create', [
+                'task' => $task
+            ]);
+    }
+        
+    public function store(Request $request)
+    {
+        $task = new task;
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+    
+    public function edit($id)
+    {
+        $task = task::find($id);
+
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+    
+    public function update(Request $request, $id)
+    {
+        $task = task::find($id);
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+    
+    public function destroy(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return redirect('/');
+    }
 }
